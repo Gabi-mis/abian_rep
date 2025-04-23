05763b54fff9b04785658027bf863c1b515bf0f2
Gabi-mis <kx410tklymipene@gmail.com>
2025-04-23 15:40:31 +0100

    Update resumen.py

--- resumen.py
+++ resumen.py
@@ -17,6 +17,11 @@ get_value() {
     xmlstarlet sel -N s="$ns" -t -v "$path" -n "$XML_FILE"
 }
 
+# ----- Extraer el último timestamp (registro más reciente) -----
+# Encuentra el último timestamp en el XML
+latest_timestamp=$(xmlstarlet sel -N s="$ns" -t -v "(//s:timestamp)[last()]/@date" "$XML_FILE")
+latest_time=$(xmlstarlet sel -N s="$ns" -t -v "(//s:timestamp)[last()]/@time" "$XML_FILE")
+
 # ----- Datos del sistema -----
 sysdata_version=$(get_value "//s:sysdata-version")
 sysname=$(get_value "//s:sysname")
@@ -27,10 +32,8 @@ num_cpus=$(get_value "//s:number-of-cpus")
 # ----- Fecha y hora del archivo -----
 file_date=$(get_value "//s:file-date")
 file_time=$(get_value "//s:file-utc-time")
-timestamp_date=$(get_value "//s:timestamp/@date")
-timestamp_time=$(get_value "//s:timestamp/@time")
 
-# ----- Uso de CPU -----
+# ----- Uso de CPU (último registro) -----
 cpu_user=$(get_value "//s:cpu[@number='all']/@user")
 cpu_system=$(get_value "//s:cpu[@number='all']/@system")
 cpu_nice=$(get_value "//s:cpu[@number='all']/@nice")
@@ -38,8 +41,8 @@ cpu_iowait=$(get_value "//s:cpu[@number='all']/@iowait")
 cpu_steal=$(get_value "//s:cpu[@number='all']/@steal")
 cpu_idle=$(get_value "//s:cpu[@number='all']/@idle")
 
-# ----- Mostrar resumen -----
-echo "Resumen del registro sysstat:"
+# ----- Mostrar resumen del último registro -----
+echo "Resumen del registro sysstat (último registro):"
 echo "------------------------------------"
 echo "Versión y sistema:"
 echo "  sysstat version:  $sysdata_version"
@@ -49,11 +52,14 @@ echo "  Arquitectura:     $machine"
 echo "  Núcleos de CPU:   $num_cpus"
 echo
 echo "Fecha del archivo:"
-echo "  Fecha de registro:     $file_date"
-echo "  Hora UTC del registro: $file_time"
-echo "  Timestamp del snapshot: $timestamp_date $timestamp_time"
+echo "  Fecha de archivo:     $file_date"
+echo "  Hora UTC de archivo:  $file_time"
 echo
-echo "Uso de CPU:"
+echo "Fecha del último registro (timestamp):"
+echo "  Fecha de registro:     $latest_timestamp"
+echo "  Hora UTC del registro: $latest_time"
+echo
+echo "Uso de CPU del último registro:"
 echo "  User:   $cpu_user%"
 echo "  System: $cpu_system%"
 echo "  Nice:   $cpu_nice%"
@@ -61,3 +67,4 @@ echo "  IOWait: $cpu_iowait%"
 echo "  Steal:  $cpu_steal%"
 echo "  Idle:   $cpu_idle%"
 echo "------------------------------------"
+
